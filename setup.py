39a45458188f64d7526b5c1feaeef4fb91c71b4d
Jakub Dorňák <jdornak@redhat.com>
2015-05-04 19:40:54 +0200

    add Installation steps to README.md

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name         = 'django-fas',
-    version      = '1.0.1',
+    version      = '1.0.2',
     description  = 'Django auth backend for FAS (Fedora Accounts System)',
     author       = 'Jakub Dorňák',
     author_email = 'jdornak@redhat.com',
