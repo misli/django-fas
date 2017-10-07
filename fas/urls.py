958684dc3cec2e6387aece112dfeddca0687ec95
Jakub Dorňák <jakub.dornak@misli.cz>
2017-10-07 23:43:43 +0200

    newer versions of Django do not provide django.conf.urls.patterns

--- fas/urls.py
+++ fas/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import login, complete, logout
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^login/$',    login,      name='fas-login'),
     url(r'^complete/$', complete,   name='fas-complete'),
     url(r'^logout/$',   logout,     name='fas-logout'),
-)
+]
