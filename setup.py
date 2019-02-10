8ff058c06f60030af3ccf7be2d19abb4f49fed09
Jakub Dorňák <jakub.dornak@misli.cz>
2019-02-10 21:13:15 +0100

    Python 2 is actually not supported
    resolves: #4

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
     ],
 )
